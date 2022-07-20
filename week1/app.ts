8ea6c71e7b15921f656e108ee83c84a7d91c9e73
ukkary <ukkary@gmail.com>
2022-07-21 03:57:08 +0900

    update validation

--- week1/app.ts
+++ week1/app.ts
@@ -21,14 +21,19 @@ const question = `最初は、${handEmoji.rock}！ジャンケン......!?\n0: ${
 const computerHandMap = [handEmoji.rock, handEmoji.scissors, handEmoji.paper]
 
 std.question(question, (line) => {
-  const computerHand = Math.floor(Math.random() * 3);
-  const playerHand = Number(line);
-  console.log("--------------------------");
-  console.log(`あなたの手: ${computerHandMap[playerHand]}`);
-  console.log(`コンピューターの手: ${computerHandMap[computerHand]}`);
-  computerHand === playerHand && console.log(`結果: あいこ${resultEmoji.draw}`);
-  (computerHand - playerHand + 3) % 3 === 1 && console.log(`結果: 勝ち${resultEmoji.win}`);
-  (computerHand - playerHand + 3) % 3 === 2 && console.log(`結果: 負け${resultEmoji.lose}`);
-  console.log("--------------------------");
-  std.close();
+  try {
+    const computerHand = Math.floor(Math.random() * 3);
+    const playerHand = Number(line);
+    console.log("--------------------------");
+    console.log(`あなたの手: ${computerHandMap[playerHand]}`);
+    console.log(`コンピューターの手: ${computerHandMap[computerHand]}`);
+    computerHand === playerHand && console.log(`結果: あいこ${resultEmoji.draw}`);
+    (computerHand - playerHand + 3) % 3 === 1 && console.log(`結果: 勝ち${resultEmoji.win}`);
+    (computerHand - playerHand + 3) % 3 === 2 && console.log(`結果: 負け${resultEmoji.lose}`);
+    console.log("--------------------------");
+    std.close();
+  } catch (error) {
+    console.log("無効試合")
+    std.close();
+  }
 });
